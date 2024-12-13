75e7cb90d07d708e1852761c2b41464bde6645cd
rezvan khalili <rezvan.kh.1999@gmail.com>
2024-12-13 00:59:14 -0800

    making the files

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+Do you remember the game of hop?
+
+Get a  number between 2 and 6 from the user(you can use prompt and alert in this practice), then log from 1 to 200 in the console.
+It is just like the game of hop, whatever number the user enters, the numbers will be printed accordingly.
+
+Example: If the user enters the number 3, the numbers will be printed like this:
+1, 2, hop, 4, 5, hop, 7, 8, hop, 10 ..., 200 
+*/
+//! Answer:
